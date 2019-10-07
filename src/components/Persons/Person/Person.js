71dc5470af15ade1d1d825a161c60e0f424b0956
ToniMihajlovski <toni@sopro.io>
2019-10-07 16:26:59 +0200

    Update LigeCycle for props change

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Person.css';
 
-const person = (props) => {
-   console.log('[Person.js] rendering....');
-    return (
-        <div className="Person">
-            <p onClick={props.click}>I'm {props.name} and I'm {props.age} Old, and this is a random number {Math.floor(Math.random() * 30)} </p>
-            <p>{props.children}</p>
-             <input type="text" onChange={props.changed} value={props.name}/>   
-        </div>   
-    )
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering....');
+         return (
+             <div className="Person">
+                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} Old, and this is a random number {Math.floor(Math.random() * 30)} </p>
+                 <p>{this.props.children}</p>
+                  <input type="text" onChange={this.props.changed} value={this.props.name}/>   
+             </div>   
+         )
+    }
 }
 
-export default person;
+export default Person;
