27d5c08f41cd43fea64b982a09a14d38ffb16067
ToniMihajlovski <toni@sopro.io>
2019-10-26 15:30:12 +0200

    Using Axilliary component wit children property

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
+import Auxilliary from '../../../hoc/Auxilliary.js'
 import './Person.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering....');
-         return [
-                 <p key='i1' onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} Old, and this is a random number {Math.floor(Math.random() * 30)} </p>,
-                 <p key='i2'>{this.props.children}</p>,
-                  <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}/>   
-                ]
+         return (
+            <Auxilliary>
+                <p  onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} Old, and this is a random number {Math.floor(Math.random() * 30)} </p>
+                <p >{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>   
+            </Auxilliary>
+         )               
     }
 }
 
