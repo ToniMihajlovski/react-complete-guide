9b2e3314289b01d1606cd42e8a990acea2d946fd
ToniMihajlovski <toni@sopro.io>
2019-10-26 15:17:00 +0200

    using an array for adjacent Elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,11 @@ import './Person.css';
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering....');
-         return (
-             <div className="Person">
-                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} Old, and this is a random number {Math.floor(Math.random() * 30)} </p>
-                 <p>{this.props.children}</p>
-                  <input type="text" onChange={this.props.changed} value={this.props.name}/>   
-             </div>   
-         )
+         return [
+                 <p key='i1' onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} Old, and this is a random number {Math.floor(Math.random() * 30)} </p>,
+                 <p key='i2'>{this.props.children}</p>,
+                  <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}/>   
+                ]
     }
 }
 
